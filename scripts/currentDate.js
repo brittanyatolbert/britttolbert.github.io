3909e0d4b078b76abac548c99970c5ea525591a6
brittanyatolbert <brittanyatolbert@gmail.com>
2020-01-14 13:42:50 -0700

    commit

--- scripts/currentDate.js
+++ scripts/currentDate.js
@@ -4,5 +4,9 @@ console.log(currentDate);
 const currentYear = currentDate.getFullYear();
 console.log(currentYear);
 
+var lastModified = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = lastModified;
+
 document.getElementById("currentYear").innerHTML = currentYear;
 
