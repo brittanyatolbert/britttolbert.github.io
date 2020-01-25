f60ca9899f0c9564e4abee52de07b54713788938
brittanyatolbert <brittanyatolbert@gmail.com>
2020-01-25 14:55:16 -0700

    c

--- lesson3/js/currentYear.js
+++ lesson3/js/currentYear.js
@@ -4,4 +4,8 @@ console.log(currentDate);
 const currentYear = currentDate.getFullYear();
 console.log(currentYear);
 
+var lastModified = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = lastModified;
+
 document.getElementById("currentYear").innerHTML = currentYear;
