7d89bf3b3a461f3f7573d9337671938f0675c124
brittanyatolbert <brittanyatolbert@gmail.com>
2020-02-06 10:55:27 -0700

    c

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -64,3 +64,14 @@ function toggleMenu() {
 document.querySelector('#current-temp').innerHTML = '50&deg;';
 document.querySelector('#wind-speed').innerHTML = '10mph';
 document.querySelector('#humidity').innerHTML = '10&percnt;';
+
+/*Friday Banner*/
+
+ const pancake = new Date();
+ const aside = document.querySelector('aside');
+ if (pancake.getDay() === 5) {
+    aside.style.display = 'block';
+ }
+ else {
+   aside.style.display = 'none';
+ }
