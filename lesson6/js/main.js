29a1808a7c8e4dcfcee20895179c3df7d28e6a5a
brittanyatolbert <brittanyatolbert@gmail.com>
2020-02-13 10:23:17 -0700

    c

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -61,9 +61,21 @@ function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
 }
 
-document.querySelector('#current-temp').innerHTML = '50&deg;';
-document.querySelector('#wind-speed').innerHTML = '10mph';
-document.querySelector('#humidity').innerHTML = '10&percnt;';
+/*weather summary*/
+let f, t, s;
+t = 50;
+s = 10;
+
+if ( t <= 50 && s >= 3) {
+  f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+}
+else {
+  f = 'N/A'
+  document.querySelector('#humidity').innerHTML = f;
+}
+
+document.querySelector('#current-temp').innerHTML = t + '&deg;'; //'50&deg;';
+document.querySelector('#wind-speed').innerHTML = s + 'mph';
 
 /*Friday Banner*/
 
