47a705d217ff438d30ffa1690efd517db6380baa
brittanyatolbert <brittanyatolbert@gmail.com>
2020-03-03 10:49:40 -0700

    c

--- lesson9/json/prophets.js
+++ lesson9/json/prophets.js
@@ -9,13 +9,27 @@ fetch('https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.
 
         prophets.forEach(prophet => {
                 let article = document.createElement('article');
-                
+
                 let h1 = document.createElement('h1');
-                h1.textContent = '${prophet.name} ${prophet.lastname}';
+                h1.textContent = `${prophet.name} ${prophet.lastname}`;
 
-                article.appendChild(h1);
+                let dateOfBirth = document.createElement('p');
+                dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
 
-                document.querySelector('#prophets')[0].appendChild('article');
+                let placeOfBirth = document.createElement('p');
+                placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+                let image = document.createElement('img');
+                image.setAttribute('src', prophet.imageurl);
+                image.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
+
+
+                article.appendChild('h1');
+                article.appendChild(dateOfBirth);
+                article.appendChild(placeOfBirth);
+                article.appendChild(image);
+
+                document.querySelector('#prophets').appendChild(article);
 
 
             })
