4a92fccb1ca63289e882b220361811145523193f
brittanyatolbert <brittanyatolbert@gmail.com>
2020-03-03 10:37:30 -0700

    c

--- lesson9/json/prophets.js
+++ lesson9/json/prophets.js
@@ -8,7 +8,17 @@ fetch('https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.
         let prophets = prophetList.prophets;
 
         prophets.forEach(prophet => {
-                console.log(prophet.name + ' ' + prophet.lastname);
+                let article = document.createElement('article');
+                
+                let h1 = document.createElement('h1');
+                h1.textContent = '${prophet.name} ${prophet.lastname}';
+
+                article.appendChild(h1);
+
+                document.querySelector('#prophets')[0].appendChild('article');
+
+
             })
+
 
         });
