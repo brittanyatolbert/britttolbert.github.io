0449fed2dc37e6b23723463659ebc026ad1191cf
brittanyatolbert <brittanyatolbert@gmail.com>
2020-03-05 11:15:02 -0700

    c

--- lesson9/js/json.js
+++ lesson9/js/json.js
@@ -7,16 +7,29 @@ fetch ('https://byui-cit230.github.io/weather/data/towndata.json')
 .then(resultJSON => {
     let towns= resultJSON.towns;
 
-    towns.forEach();
+    towns.forEach(
         town => {
             if (town.name === 'Fish Haven') {
-                document.querySelector('fishHavenName').textContent = town.name;
-                document.querySelector('fishHavenMotto').textContent = town.motto;
-                document.querySelector('fishHavenYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
-                document.querySelector('fishHavenPopulation').textContent = `Population: ${town.currentPopulation}`;
-                document.querySelector('fishHavenAnnualRainfall').textContent = `Annaul Rainfall: ${town.averageRainfall}`;
-                document.querySelector('fishHavenImage').setAttribute('src', `images/${town.photo}`);
-                document.querySelector('fishHavenImage').setAttribute('alt', `images/${town.name}`);
+                document.querySelector('#fishHavenMotto').textContent = town.motto;
+                document.querySelector('#fishHavenYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
+                document.querySelector('#fishHavenPopulation').textContent = `Population: ${town.currentPopulation}`;
+                document.querySelector('#fishHavenAnnualRainfall').textContent = `Annaul Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#fishHavenImage').setAttribute('src', `image/${town.photo}`);
+                document.querySelector('#fishHavenImage').setAttribute('alt', `image/${town.name}`);
+            } else if (town.name === 'Preston') {
+                document.querySelector('#prestonMotto').textContent = town.motto;
+                document.querySelector('#prestonYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
+                document.querySelector('#prestonPopulation').textContent = `Population: ${town.currentPopulation}`;
+                document.querySelector('#prestonAnnualRainfall').textContent = `Annaul Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#prestonImage').setAttribute('src', `image/${town.photo}`);
+                document.querySelector('#prestonImage').setAttribute('alt', `image/${town.name}`);
+            } else if (town.name === 'Soda Springs'){
+                document.querySelector('#sodaSpringsMotto').textContent = town.motto;
+                document.querySelector('#sodaSpringsYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
+                document.querySelector('#sodaSpringsPopulation').textContent = `Population: ${town.currentPopulation}`;
+                document.querySelector('#sodaSpringsAnnualRainfall').textContent = `Annaul Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#sodaSpringsImage').setAttribute('src', `image/${town.photo}`);
+                document.querySelector('#sodaSpringsImage').setAttribute('alt', `image/${town.name}`);
             }
-    }
+        });
 })
