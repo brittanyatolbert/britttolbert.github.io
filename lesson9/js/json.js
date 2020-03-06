d289e5c5ac2bf8b57ec93977e420cea476a832a6
brittanyatolbert <brittanyatolbert@gmail.com>
2020-03-06 12:11:45 -0700

    c

--- lesson9/js/json.js
+++ lesson9/js/json.js
@@ -13,21 +13,21 @@ fetch ('https://byui-cit230.github.io/weather/data/towndata.json')
                 document.querySelector('#fishHavenMotto').textContent = town.motto;
                 document.querySelector('#fishHavenYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
                 document.querySelector('#fishHavenPopulation').textContent = `Population: ${town.currentPopulation}`;
-                document.querySelector('#fishHavenAnnualRainfall').textContent = `Annaul Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#fishHavenAnnualRainfall').textContent = `Annual Rainfall: ${town.averageRainfall}`;
                 document.querySelector('#fishHavenImage').setAttribute('src', `image/${town.photo}`);
                 document.querySelector('#fishHavenImage').setAttribute('alt', `image/${town.name}`);
             } else if (town.name === 'Preston') {
                 document.querySelector('#prestonMotto').textContent = town.motto;
                 document.querySelector('#prestonYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
                 document.querySelector('#prestonPopulation').textContent = `Population: ${town.currentPopulation}`;
-                document.querySelector('#prestonAnnualRainfall').textContent = `Annaul Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#prestonAnnualRainfall').textContent = `Annual Rainfall: ${town.averageRainfall}`;
                 document.querySelector('#prestonImage').setAttribute('src', `image/${town.photo}`);
                 document.querySelector('#prestonImage').setAttribute('alt', `image/${town.name}`);
             } else if (town.name === 'Soda Springs'){
                 document.querySelector('#sodaSpringsMotto').textContent = town.motto;
                 document.querySelector('#sodaSpringsYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
                 document.querySelector('#sodaSpringsPopulation').textContent = `Population: ${town.currentPopulation}`;
-                document.querySelector('#sodaSpringsAnnualRainfall').textContent = `Annaul Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#sodaSpringsAnnualRainfall').textContent = `Annual Rainfall: ${town.averageRainfall}`;
                 document.querySelector('#sodaSpringsImage').setAttribute('src', `image/${town.photo}`);
                 document.querySelector('#sodaSpringsImage').setAttribute('alt', `image/${town.name}`);
             }
